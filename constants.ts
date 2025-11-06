ecb9fe02ac52300920d5938fc08aca8e5a41beb8
badacorgi <154527537+badacorgi@users.noreply.github.com>
2025-11-06 16:06:23 +0900

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -1,5 +1,8 @@
 import { Project } from './types';
-
+import { Project } from './types';
+import Thumb1 from './thumb1.jpg';
+import Thumb2 from './thumb2.jpg';
+import Thumb3 from './thumb3.jpg';
 // 여기에 학생들의 프로젝트 정보를 추가하거나 수정하세요.
 // 썸네일과 링크를 추가하면 자동으로 웹페이지에 반영됩니다.
 // 예시: 
@@ -11,21 +14,21 @@ export const PROJECTS: Project[] = [
     id: 1,
     title: '한붓그리기 퍼즐',
     description: '하나의 연속된 선으로 모든 점을 연결하는 퍼즐 게임입니다. 도전해보세요!',
-    thumbnailUrl: '/thumb1.jpg', // Provided image uploaded for thumbnail
+    thumbnailUrl: Thumb1, // Provided image uploaded for thumbnail
     projectUrl: 'https://famous-kitten-1bfb11.netlify.app/',
   },
   {
     id: 2,
     title: 'HunterCatcher',
     description: '흥미진진한 슈팅게임 적들을 물리치고 새로운 무기를 획득하세요',
-    thumbnailUrl: '/thumb2.jpg', // 예: 'thumb2.jpg'
+    thumbnailUrl: Thumb2, // 예: 'thumb2.jpg'
     projectUrl: 'https://profound-creponne-5fe9ca.netlify.app/',   // 예: 'link2'
   },
   {
     id: 3,
     title: '나만의 고양이 만들기',
     description: '나만의 고양이를 만들고 키우는 게임입니다.',
-    thumbnailUrl: '/thumb3.jpg', // 예: 'thumb3.jpg'
+    thumbnailUrl: Thumb3, // 예: 'thumb3.jpg'
     projectUrl: 'https://warm-taiyaki-48136b.netlify.app',   // 예: 'link3'
   },
   {
